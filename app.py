4f14fc50edabefed8cc9e73317fbfbfcdee999dd
knishk <96434556+k4ndy-1@users.noreply.github.com>
2023-09-05 18:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
+import pandas as pd  #import libraries
 import time
 import plotly.express as px
 
 
-st.set_page_config(
+st.set_page_config(  # setup the webpage 
     page_title="Bank Data Management",
     page_icon=":bar_chart:",
     layout="wide"
@@ -20,24 +20,30 @@ st.dataframe(df)
 #Sidebar
 
 st.sidebar.header("Please Filter it here")
-job=st.sidebar.multiset(
+job=st.sidebar.multiselect(
     "Select the job:",
     options=df["job"].unique(),
     default=df["job"].unique()
 )
 
-loan=st.sidebar.multiset(
+loan=st.sidebar.multiselect(
     "Select the entries with loan:",
     options=df["loan"].unique(),
     default=df["loan"].unique()
 )
 
-month=st.sidebar.multiset(
+month=st.sidebar.multiselect(  # we filter the data on basis of month ,loan and job by pandas library
     "Select the entries with specific month",
     options=df["month"].unique(),
     default=df["month"].unique()
 )
 
+df_selection=df.query(  # return filtered dataset to our webapp instead of all the data
+    "City == @city & Customer_type == @customer_type & Gender == @gender"
+)
+
+
+st.dataframe(df_selection)
 
 
 
