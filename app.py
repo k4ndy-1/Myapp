a24c38ca1941e45b1431fd90016369fa96adfcfd
knishk <96434556+k4ndy-1@users.noreply.github.com>
2023-09-05 19:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,32 @@ df_selection=df.query(
     "job==@Job & loan==@Loan & month==@Month"
 )
 
-st.dataframe(df_selection)
+# ######MAINPAGE#########
+
+st.title(":dollar_banknote: Loan Retrieval Information")
+st.markdown("##")
+
+#KPI
+
+total_loan=int(df_selection["loan"].sum())
+median_balance=int(df_selection["balance"].median()]
+leverage=int((df_selection["loan"])/(df_selection["balance"]))
 
 
+
+left_col,mid_col,right_col =st.columns(3)
+
+with left_col:
+    st.subheader("Total loan")
+    st.subheader(f"INR {total_loan:,}")
+with mid_col:
+    st.subheader("Mean Balance")
+    st.subheader(f"INR {median_balance:,}")
+with right_col:
+    st.subheader("leverage")
+    st.subheader(f"INR {leverage:,}")
+
+st.markdown("---") # divider
 
 
 
