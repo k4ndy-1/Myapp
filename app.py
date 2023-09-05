ce7a04c767b2566acdf0c0b4b42ce53113f7f4cd
knishk <96434556+k4ndy-1@users.noreply.github.com>
2023-09-05 20:00:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.markdown("##")
 
 #KPI
 
-total_loan=int(df_selection["loan"].sum())
+total_loan=df_selection["loan"].sum()
 '''median_balance=int(df_selection["balance"].median())
 leverage=int((df_selection["loan"])/(df_selection["balance"]))'''
 
